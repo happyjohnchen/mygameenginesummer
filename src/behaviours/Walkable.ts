c3e9cc2a6f07acaa357b357fdb5cfe67a6581b5b
陈沫言 <ab112334@163.com>
2022-06-21 20:41:55 +0800

    feat(Walkable):向Walkable组件添加了移动方向

--- src/behaviours/Walkable.ts
+++ src/behaviours/Walkable.ts
@@ -1,11 +1,21 @@
 import { Behaviour } from "../engine/Behaviour";
 import { Transform } from "../engine/Transform";
 import { number } from "../engine/validators/number";
+import {string} from "../engine/validators/string";
 
 export class Walkable extends Behaviour {
     private currentSpeed = 0;
     @number()
     speed = 1;
+    @string({
+        editorType: 'select', options: [
+            {value:'up', label:"向上"},
+            {value:'down', label:"向下"},
+            {value:'left', label:"向左"},
+            {value:'right', label:"向右"}
+        ]
+    })
+    direction = "right"
 
     onStart() {
         this.gameObject.onClick = () => {
@@ -19,6 +29,20 @@ export class Walkable extends Behaviour {
 
     onTick(duringTime: number) {
         const transform = this.gameObject.getBehaviour(Transform);
-        transform.x += this.currentSpeed;
+        switch (this.direction){
+            case 'up':
+                transform.y -= this.currentSpeed;
+                break;
+            case "down":
+                transform.y += this.currentSpeed;
+                break;
+            case "left":
+                transform.x -= this.currentSpeed;
+                break;
+            case "right":
+                transform.x += this.currentSpeed;
+                break;
+
+        }
     }
 }
