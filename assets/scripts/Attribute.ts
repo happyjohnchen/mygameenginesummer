53f74e349724da33434d642a7a7e49a798d1fb5a
许佳阳 <2591483579@qq.com>
2022-06-29 14:46:48 +0800

    更改了消耗算法 创建了初级room类 更改了添加属性

--- assets/scripts/Attribute.ts
+++ assets/scripts/Attribute.ts
@@ -7,10 +7,11 @@ import { string } from "../../src/engine/validators/string";
 export class Attribute extends Behaviour {
 
     //在此定义脚本中的属性
-    /* 随时间调落数值*/
+    /* 分别挂在到三个属性的物体上，用来实现随时间掉落 且控制上下限
+    */
 
    
-     private value = 60;//初始数值百分比
+     private value = 60;//初始数值
      private gameobject;
 
      @number()
@@ -19,34 +20,32 @@ export class Attribute extends Behaviour {
      private lasttime = 0;//经过的时间
      private nowtime = 0;
      @number()
-     minpertime = 2;//多少分钟消耗一次
+     oncetime = 1;//多少小时消耗一次
 
 
      maxvalue = 100;
      minvalue= 0;
 
     //游戏开始时会执行一次
-    onStart() { //基础数值
+    onStart() { //游戏开始时的数值
         
         this.gameObject.getBehaviour(TextRenderer).text= this.value.toString();
     }
 
-    //每次屏幕刷新执行
+    //每次屏幕刷新执行 显示数值
     onUpdate() {
         this.gameObject.getBehaviour(TextRenderer).text= this.value.toString();
     }
 
-    //平均每16ms执行一次 拿到时间
+    //平均每16ms执行一次 每一小时掉一次
     onTick(duringTime: number) {
-        this.nowtime= getGameObjectById('TimeController').getBehaviour(TimeControllerSystem).getMinTime();
-        this.lasttime = this.lasttime==60? 0:this.lasttime;
-        if(this.nowtime-this.lasttime >=this. minpertime){
+        this.nowtime= getGameObjectById('TimeController').getBehaviour(TimeControllerSystem).getHourTime();
+        this.lasttime = this.lasttime==16? 0:this.lasttime;
+        if(this.nowtime-this.lasttime >=this. oncetime){
             this.setvalue(-this.consumepertime);
             this.lasttime = this.nowtime;    
             console.log(this.lasttime);
         }
-        
-        this.onUpdate();
         //console.log(this.lasttime);
         
     }
