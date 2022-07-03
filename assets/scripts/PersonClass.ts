984a1667ff3a7df24b22556539414d010bf0eb08
Ryannn <10461044+chen_ryannn@user.noreply.gitee.com>
2022-07-03 18:17:58 +0800

    add Behaviours

---
+++ assets/scripts/PersonClass.ts
@@ -0,0 +1,42 @@
+import { Behaviour } from "../../src/engine/Behaviour";
+
+export class PersonClass extends Behaviour {
+
+    //在此定义脚本中的属性
+    personId: number
+    personName: string
+    room: Behaviour
+
+    //游戏编辑模式或运行模式开始时会执行一次
+    onStart() {
+
+    }
+
+    //游戏运行模式开始时会执行一次
+    onPlayStart() {
+        this.gameObject.onClick = () => {
+            //移动房间
+        }
+    }
+
+    //每次屏幕刷新执行
+    onUpdate() {
+
+    }
+
+    //平均每16ms执行一次
+    onTick(duringTime: number) {
+
+    }
+
+    //删除Behaviour时会执行一次
+    onEnd() {
+
+    }
+
+    setAnimation(){
+        
+    }
+
+    
+}
---
+++ assets/scripts/PersonSet.ts
@@ -0,0 +1,51 @@
+import { GameObject } from "../../src/engine";
+import { Behaviour } from "../../src/engine/Behaviour";
+import { PersonClass } from "./PersonClass";
+import { GameController } from "./GameController";
+
+export class PersonSet extends Behaviour {
+
+    //在此定义脚本中的属性
+    time: number;
+    peopleCount: number;
+    gameController: GameController
+
+
+
+    //游戏编辑模式或运行模式开始时会执行一次
+    onStart() {
+        this.gameController = new GameController();
+        this.gameController.addPerson(this.gameObject);
+    }
+
+    //游戏运行模式开始时会执行一次
+    onPlayStart() {
+
+    }
+
+    //每次屏幕刷新执行
+    onUpdate() {
+
+    }
+
+    //平均每16ms执行一次
+    onTick(duringTime: number) {
+        //if(this.time);
+
+    }
+
+    //删除Behaviour时会执行一次
+    onEnd() {
+
+    }
+
+    newPerson() {
+        const newPerson = new GameObject();
+        this.gameObject.addChild(newPerson);
+        const personTnfomation = new PersonClass();
+        //personTnfomation.personId 
+
+
+    }
+    
+}
