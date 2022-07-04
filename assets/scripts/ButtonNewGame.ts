6d992457eccc7f86b516ec00f86d97a2f9c2d35c
陈沫言 <ab112334@163.com>
2022-07-04 10:24:51 +0800

    添加菜单按钮Behaviour

---
+++ assets/scripts/ButtonNewGame.ts
@@ -0,0 +1,36 @@
+import {Behaviour} from "../../src/engine/Behaviour";
+
+export class ButtonNewGame extends Behaviour {
+
+    //在此定义脚本中的属性
+
+
+    //游戏编辑模式或运行模式开始时会执行一次
+    onStart() {
+
+    }
+
+    //游戏运行模式开始时会执行一次
+    onPlayStart() {
+        this.gameObject.onClick = () => {
+            console.log("新游戏");
+            //开始新游戏
+            this.engine.loadScene('assets/scenes/game.yaml', '');
+        }
+    }
+
+    //每次屏幕刷新执行
+    onUpdate() {
+
+    }
+
+    //平均每16ms执行一次
+    onTick(duringTime: number) {
+
+    }
+
+    //删除Behaviour时会执行一次
+    onEnd() {
+
+    }
+}
