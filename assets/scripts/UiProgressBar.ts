17ba1638932312298eec723d9ff5ce6a8d2fe858
陈沫言 <ab112334@163.com>
2022-07-06 10:21:23 +0800

    加入进度条

---
+++ assets/scripts/UiProgressBar.ts
@@ -0,0 +1,75 @@
+import {Behaviour} from "../../src/engine/Behaviour";
+import {GameController} from "./GameController";
+import {AttributeSystem} from "./AttributeSystem";
+import {ShapeRectRenderer} from "../../src/behaviours/ShapeRectRenderer";
+import {GameObject, getGameObjectById} from "../../src/engine";
+import {TextRenderer} from "../../src/behaviours/TextRenderer";
+
+export class UiProgressBar extends Behaviour {
+
+    //在此定义脚本中的属性
+    gameController: GameController
+    attributeSystem: AttributeSystem
+    energyBar: ShapeRectRenderer
+    foodBar: ShapeRectRenderer
+    waterBar: ShapeRectRenderer
+    energyText: GameObject
+    foodText: GameObject
+    waterText: GameObject
+
+    showText = false;
+
+    //游戏编辑模式或运行模式开始时会执行一次
+    onStart() {
+
+    }
+
+    //游戏运行模式开始时会执行一次
+    onPlayStart() {
+        //获取所有对象
+        this.gameController = getGameObjectById('GameController').getBehaviour(GameController);
+        this.attributeSystem = getGameObjectById("AttributeSystem").getBehaviour(AttributeSystem);
+        this.energyBar = getGameObjectById('EnergyBar').getBehaviour(ShapeRectRenderer);
+        this.foodBar = getGameObjectById('FoodBar').getBehaviour(ShapeRectRenderer);
+        this.waterBar = getGameObjectById('WaterBar').getBehaviour(ShapeRectRenderer);
+        this.energyText = getGameObjectById('EnergyText');
+        this.foodText = getGameObjectById('FoodText');
+        this.waterText = getGameObjectById('WaterText');
+
+        //隐藏数字
+        this.energyText.active = this.showText;
+        this.foodText.active = this.showText;
+        this.waterText.active = this.showText;
+
+        //单击隐藏/显示数字
+        this.gameObject.onClick = () => {
+            this.showText = !this.showText;
+            this.energyText.active = this.showText;
+            this.foodText.active = this.showText;
+            this.waterText.active = this.showText;
+        }
+    }
+
+    //每次屏幕刷新执行
+    onUpdate() {
+        //更新数字显示
+        this.energyText.getBehaviour(TextRenderer).text = this.gameController.game.energy + "/" + this.attributeSystem.maxValue;
+        this.foodText.getBehaviour(TextRenderer).text = this.gameController.game.food + "/" + this.attributeSystem.maxValue;
+        this.waterText.getBehaviour(TextRenderer).text = this.gameController.game.water + "/" + this.attributeSystem.maxValue;
+
+        //更新进度条宽度
+        this.energyBar.width = this.gameController.game.energy / this.attributeSystem.maxValue * 114;
+        this.foodBar.width = this.gameController.game.food / this.attributeSystem.maxValue * 114;
+        this.waterBar.width = this.gameController.game.water / this.attributeSystem.maxValue * 114;
+    }
+
+    //平均每16ms执行一次
+    onTick(duringTime: number) {
+
+    }
+
+    //删除Behaviour时会执行一次
+    onEnd() {
+
+    }
+}
