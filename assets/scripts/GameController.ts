81074b724cc8a4525c17e77796f3e94fdf21a085
许佳阳 <2591483579@qq.com>
2022-07-07 20:54:33 +0800

    加了站立动画 调整了参数

--- assets/scripts/GameController.ts
+++ assets/scripts/GameController.ts
@@ -106,7 +106,7 @@ export class GameController extends Behaviour {
         //设定时间
         console.log(this.game)
         this.game.time.setSpeed(1.0);
-        this.game.time.setInitialTime(1, 15, 0, 0);
+        this.game.time.setInitialTime(1, 0, 0, 0);
         //设定人列表为空
         this.game.people = [];
         //设定房间有一个大门
@@ -115,7 +115,7 @@ export class GameController extends Behaviour {
         this.game.water = 50;
         this.game.energy = 50;
         this.game.food = 50;
-        this.game.material = 200;
+        this.game.material = 500;
         console.log("GameController: 新存档创建成功");
     }
 
