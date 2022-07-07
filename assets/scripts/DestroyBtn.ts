e815682a15125b7221938db4b9faab4c4fc9909e
许佳阳 <2591483579@qq.com>
2022-07-07 15:14:15 +0800

    加了个房间二级ui实现方法

--- assets/scripts/DestroyBtn.ts
+++ assets/scripts/DestroyBtn.ts
@@ -1,6 +1,7 @@
+import { getGameObjectById } from "../../src/engine";
 import {Behaviour} from "../../src/engine/Behaviour";
 
-export class BehaviourDemo extends Behaviour {
+export class DestroyBtn extends Behaviour {
 
     //在此定义脚本中的属性
 
@@ -12,7 +13,17 @@ export class BehaviourDemo extends Behaviour {
 
     //游戏运行模式开始时会执行一次
     onPlayStart() {
-
+        getGameObjectById("RoomSecondUi").active = false; 
+        this.gameObject.onClick = (e) => {
+            if(e.button ==0){//点击调用选择房间 同时关闭ui
+                this.destroyRoom();
+                getGameObjectById("RoomSecondUi").active = false;
+            }
+        }
+    }
+    destroyRoom() {
+        //这里写调用房间摧毁
+        console.log("摧毁");
     }
 
     //每次屏幕刷新执行
